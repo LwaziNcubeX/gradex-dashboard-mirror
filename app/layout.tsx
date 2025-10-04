465395059f0ef8b95d77876b232e37552678092c
Lwazi Ncube <lwazincubex@gmail.com>
2025-10-04 12:07:52 +0200

    add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,25 @@
-import type React from "react"
-import type { Metadata } from "next"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import "./globals.css";
+import { inter } from "@/fonts";
 
 export const metadata: Metadata = {
   title: "GradeX Admin Dashboard",
   description: "Manage your educational platform",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={"dark " + `${inter.className}`}
+    >
       <body className="antialiased">{children}</body>
     </html>
-  )
+  );
 }
